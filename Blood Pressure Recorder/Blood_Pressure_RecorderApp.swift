fc1d2715049220df080c12ed62bf518801e43791
AshfaqMahmud <ash.mse21@gmail.com>
2022-09-23 02:04:54 +0600

    first commit

---
+++ Blood Pressure Recorder/Blood_Pressure_RecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Blood_Pressure_RecorderApp.swift
+//  Blood Pressure Recorder
+//
+//  Created by Ashfaq on 23/9/22.
+//
+
+import SwiftUI
+
+@main
+struct Blood_Pressure_RecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
